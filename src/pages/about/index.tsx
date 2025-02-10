3038fa5ab16d9a221fe32e125cebb1a34168df0c
ajasma6570 <ajasmaprince@gmail.com>
2025-02-10 16:56:20 +0530

    cleanup

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,14 +6,12 @@ import Copywright from "@/components/Copywright";
 export default function About() {
   return (
     <div className="container min-h-screen flex flex-col justify-center items-center px-4 pt-24 md:pt-32">
-      {/* About Section */}
       <motion.div
         className="container w-full space-y-6 "
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
       >
-        {/* Title */}
         <motion.h2
           className="text-2xl md:text-4xl text-brand-dark font-bold text-center"
           initial={{ opacity: 0, y: -10 }}
@@ -23,7 +21,6 @@ export default function About() {
           About Heavenstone Residency
         </motion.h2>
 
-        {/* Paragraphs */}
         {[
           "Welcome to Heavenstone Residency, your ideal home away from home! Nestled in a brand-new building, we offer fully furnished rental apartments designed for comfort and convenience. Whether you’re staying for a short visit or a long-term stay, our 1BHK and 2BHK apartments cater to all your needs. Enjoy modern amenities like high-speed Wi-Fi, a fully equipped kitchen, smart TV, washing machine, and more. With weekly cleaning, fresh linens, and optional AC units, we ensure a hassle-free living experience.",
           "For added convenience, we offer healthy meal plans crafted with no artificial ingredients, available in both ordinary and premium options. Our premium plan even allows customizations to suit your dietary preferences.",
