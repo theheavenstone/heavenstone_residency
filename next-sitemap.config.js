68808caec0e1e999e227dea48921a927019e3b1b
ajasma <ajasma@difuse.io>
2025-02-11 18:12:03 +0530

    Disable auto robots.txt generation and use manual robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://www.heavenstone.in/',
-    generateRobotsTxt: true,
+    generateRobotsTxt: false,
     sitemapSize: 5000,
+    outDir: 'public',
 };
